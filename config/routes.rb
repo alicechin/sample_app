4a22ae3ebb00b2fda3a39d3e343c277f3b9a532d
Alice Chin <alicechin@berkeley.edu>
2016-11-15 12:55:42 -0800

    Edit Project layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  root 'static_pages#home'
 
   get 'users/new'
 
@@ -11,7 +12,9 @@ Rails.application.routes.draw do
 
   get '/contact', to: 'static_pages#contact'
 
-  root 'static_pages#home'
+  post '/signup', to: 'users#create'
+
+  resources :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
